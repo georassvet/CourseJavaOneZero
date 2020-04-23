2d7813c4cababaf331a7da95ef1b5e7b215b696b
Sergey Mokhov <s.v.mokhov@gmail.com>
2020-04-23 21:33:16 +0300

    ex5

---
+++ src/Ex5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        System.out.print("Введите число: ");
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println("1 * " + i + " = " + i);
+        System.out.println("2 * " + i + " = " + 2 * i);
+        System.out.println("3 * " + i + " = " + 3 * i);
+        System.out.println("4 * " + i + " = " + 4 * i);
+        System.out.println("5 * " + i + " = " + 5 * i);
+        System.out.println("6 * " + i + " = " + 6 * i);
+        System.out.println("7 * " + i + " = " + 7 * i);
+        System.out.println("8 * " + i + " = " + 8 * i);
+        System.out.println("9 * " + i + " = " + 9 * i);
+        System.out.println("10 * " + i + " = " + 10 * i);
+    }
+}
